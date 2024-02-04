fce05f5b5aa2c24237123156dfc65aad29eee560
Tom Collins <tomascollins@gmail.com>
2024-02-04 17:41:35 -0500

    initial-commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/collinstommy/go-chat/components"
+	"github.com/collinstommy/go-chat/template"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	template.NewTemplateRenderer(e)
+
+	e.GET("/", func(c echo.Context) error {
+
+		component := components.Index("Jon")
+
+		return template.Html(c, component)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
